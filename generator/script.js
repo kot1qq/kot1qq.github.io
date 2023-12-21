48a4701f33dda907fb2c40b00eb6900af568e7a5
kot1qq <109043688+kot1qq@users.noreply.github.com>
2023-12-21 13:53:06 +0500

    Update script.js

--- generator/script.js
+++ generator/script.js
@@ -1,1 +1,17 @@
-
+function copyText(){
+ var answer = document.getElementById("did").innerHTML;
+ navigator.clipboard.writeText(answer);
+ alert("скопировано: " + answer);
+}
+function generate() {
+ return 'xxxxxxxx-xxxx-3xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g,
+ function(c) {
+ var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+ return v.toString(16);
+ });
+}
+function change(){
+let p = document.getElementById("did");
+p.innerHTML = generate();
+}
+function reload(){change();}
