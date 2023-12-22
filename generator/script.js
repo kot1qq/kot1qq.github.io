fc0c4b07823e0bf29200ec57b2aad17eee10d382
kot1qq <109043688+kot1qq@users.noreply.github.com>
2023-12-22 12:19:05 +0500

    Update script.js

--- generator/script.js
+++ generator/script.js
@@ -1,10 +1,11 @@
-function copyText(){
- var answer = document.getElementById("did").innerHTML;
- navigator.clipboard.writeText(answer);
- alert("скопировано: " + answer);
+function redirect(){location.href = '/generator2';}
+function copy(){
+ var a = document.getElementById("did").innerHTML;
+ navigator.clipboard.writeText(a);
+ alert("Скопировано: " + a);
 }
-function generate() {
- return 'xxxxxxxx-xxxx-3xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g,
+function generate(){
+ return 'xxxxxxxxxxxx4xxxyxxxxxxxxxxxxxxx'.replace(/[xy]/g,
  function(c) {
  var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
  return v.toString(16);
@@ -12,6 +13,6 @@ function generate() {
 }
 function change(){
 let p = document.getElementById("did");
-p.innerHTML = generate();
+p.innerHTML = generate().toUpperCase();
 }
 function reload(){change();}
