fdaff49cd69b1fd58ac9208aaeb17e9ac9f5cb13
kot1qq <109043688+kot1qq@users.noreply.github.com>
2024-03-01 02:47:55 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+function getDevice(){let screenWidth = window.innerWidth; let screenHeight = window.innerHeight; if(screenWidth > 999){return "pc";} else if(screenWidth < 650){return "mobile"}}; function refresh(){location.reload()}; window.onresize = refresh;
+
+let dev = getDevice(); function opt(){if(dev === "pc"){document.getElementById("bgimg").style.width = "100%"} else {document.getElementById("bgimg").style.width = "1000px"}}
